6b1dde10a0cf58cba50e0b4adc4c3fa9ec416dd3
Anton Lyxell <git@lyxell.dev>
2025-02-17 16:16:50 +0100

    refactor: Pin nix tarball hash

--- api/shell.nix
+++ api/shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4989a246d7a390a859852baddb1013f825435cee.tar.gz") {} }:
 
 pkgs.mkShell {
   nativeBuildInputs = [
